2fd1706425652e3e7858008cb8612e54eb5ca081
judo <jt.judo@mail.ru>
2021-12-12 19:09:35 +0300

    add site

---
+++ blocks/present.php
@@ -0,0 +1,33 @@
+<?php
+$present_background_image = get_field('present_background_image');
+$present_subheadline = get_field('present_subheadline');
+$present_headline = get_field('present_headline');
+$present_btn = get_field('present_btn');
+$present_text = get_field('present_text');
+?>
+
+<section class="present" style="background-image: url(<?= $present_background_image['url'] ?>)">
+    <div class="container">
+        <div class="present__block">
+            <?php if (!empty($present_subheadline)) : ?>
+                <div class="present__block-subheadline">
+                    <h6><?= $present_subheadline ?></h6>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($present_headline)) : ?>
+                <div class="present__block-headline">
+                    <h3><?= $present_headline ?></h3>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($present_text)) : ?>
+                <div class="present__block-text">
+                    <p><?= $present_text ?></p>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($present_btn)) : ?>
+                <button class="present__block-btn btn"><?= $present_btn ?></button>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+
--- front-page.php
+++ front-page.php
@@ -4,11 +4,14 @@ get_header();
 <main>
   <?php
   include 'blocks/hero.php';
-  include 'blocks/investments.php';
-  include 'blocks/complexes.php';
-  include 'blocks/purchase.php';
   include 'blocks/advantage.php';
-  include 'blocks/form.php';
+  include 'blocks/investments.php';
+  include 'blocks/projects.php';
+  include 'blocks/present.php';
+
+//  include 'blocks/complexes.php';
+//  include 'blocks/purchase.php';
+//  include 'blocks/form.php';
   ?>
 </main>
 <?php get_footer(); ?>
